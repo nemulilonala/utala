ddfcba46b8828c10e022496e8e667a8d27387af7
nemulilonala <jan_nemu@outlook.com>
2024-10-07 02:53:04 -0500

    force tickrate to 60 and make sure vsync is enabled
    to try and fix some issues with inconsistent game speeds

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
   t.window.width = 1072
   t.window.height = 603
 	t.modules.physics = false
-	t.modules.timer = false
 	t.window.title = ":3"
   t.window.resizable = true
+  t.window.vsync = true
 end
--- main.lua
+++ main.lua
@@ -1,23 +1,28 @@
 require "chr.chrloader" --handles loading of all chr lua files
 
+local tickrate = 1/60
+local accumulator = 0.0
+
 function love.load()
   --graphics
   love.graphics.setDefaultFilter("nearest", "nearest")
   windowbase = { w = 1072, h = 603}
   windowcurrent = windowbase
   windowscale = { w = 1, h = 1, x = 1}
-	floorpos = rnd(windowbase.h*(5/6))
+	floorpos = windowbase.h-100
 	startingdist = 200
 	stagewidth = windowbase.w
   pman = playermanager.new()
   pman:spawnplayers(chrcat, chrcat)
-  --rapidlyincreasingnumber = 0
 end
 
-function love.update()
-  --rapidlyincreasingnumber=rapidlyincreasingnumber+1
-  pman:beginstep()
-  pman:endstep()
+function love.update(dt)
+  accumulator = accumulator + dt
+  while accumulator > tickrate do
+      pman:beginstep()
+      pman:endstep()
+      accumulator = accumulator - tickrate
+  end
 end
 
 function love.draw()
